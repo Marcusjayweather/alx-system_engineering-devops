11482aadf2d3a36a01b1d88357a246c6a7dd2ed5
marcusjayweather <marcusjnr61@gmail.com>
2024-01-23 10:47:31 -0800

    web

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,25 @@
+# install and configure nginx using Puppet
+package { 'nginx':
+  ensure => installed,
+}
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "server {
+      listen 80;
+
+      location / {
+        return 200 'Hello World!';
+      }
+
+      location /redirect_me {
+        return 301 http://www.example.com;
+      }
+  }",
+  notify  => Service['nginx'],
+}
